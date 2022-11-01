9d20602c16245bce112678877635bbfa4f87dc4e
Matthew Ballard <matt@mrb.email>
2022-11-01 14:48:46 -0400

    remove tests

--- Hw11.lean
+++ Hw11.lean
@@ -10,35 +10,18 @@ variable { α : Type }
 
 -- Problem 1 
 
-theorem subset_refl : Reflexive (@Subset α) := fun _ => sub_refl 
+theorem subset_refl : Reflexive (@Subset α) := sorry 
 
-theorem subset_antisymm : AntiSymmetric (@Subset α) := by 
-  intro X Y h₁ h₂
-  exact set_ext.mpr (fun x => ⟨h₁ x, h₂ x⟩) 
+theorem subset_antisymm : AntiSymmetric (@Subset α) := sorry  
 
-theorem subset_trans : Transitive (@Subset α) := fun _ _ _ h₁ h₂ x h => h₂ x (h₁ x h)
+theorem subset_trans : Transitive (@Subset α) := sorry 
 
 -- Problem 2 
 
-def Rel (f : α → α) (a₁ a₂ : α) : Prop := f a₁ = a₂ 
+def Rel (f : α → α) (a₁ a₂ : α) : Prop := sorry 
 
-theorem refl_id (f : α → α) (h : Reflexive (Rel f) ) : f = id := by 
-  apply funext 
-  intro a 
-  exact h a 
+theorem refl_id (f : α → α) (h : Reflexive (Rel f) ) : f = id := sorry  
 
-theorem symm_involution (f : α → α) (h : Symmetric (Rel f)) : f ∘ f = id := by 
-  apply funext 
-  intro x 
-  have : Rel f x (f x) := by rfl 
-  have : Rel f (f x) x := h this 
-  exact this 
+theorem symm_involution (f : α → α) (h : Symmetric (Rel f)) : f ∘ f = id := sorry  
 
-theorem trans_idempotent (f : α → α) (h : Transitive (Rel f)) : f ∘ f = f := by 
-  apply funext 
-  intro x 
-  have h₁ : Rel f x (f x) := by rfl 
-  have h₂ : Rel f (f x) (f (f x)) := by rfl 
-  have h₃ : Rel f x (f (f x)) := h h₁ h₂ 
-  exact Eq.symm h₃ 
-
+theorem trans_idempotent (f : α → α) (h : Transitive (Rel f)) : f ∘ f = f := sorry  
